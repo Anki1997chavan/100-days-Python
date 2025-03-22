65cffdc60a4dec558e78153f22774b4d22a3bff9
Ankita Ashok Chavan <126692134+Anki1997chavan@users.noreply.github.com>
2025-03-22 10:17:50 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number :"))
+num2 = float(input("Enter the second number :"))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2 
+quotient = num1/ num2 
+
+print(f"Sum: {sum_result}")  
+print(f"Difference: {difference}")  
+print(f"Product: {product}")  
+print(f"Quotient: {quotient}")  
+
+
